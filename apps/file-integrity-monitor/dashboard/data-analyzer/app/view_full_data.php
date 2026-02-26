1c726fb83e1605aca77418d897b689bba48df217
Tharindu Thenuwara <tharindut@wso2.com>
2026-02-26 10:01:14 +0530

    code - rabbit update view_full_data.php

--- apps/file-integrity-monitor/dashboard/data-analyzer/app/view_full_data.php
+++ apps/file-integrity-monitor/dashboard/data-analyzer/app/view_full_data.php
@@ -16,10 +16,14 @@
  * under the License. 
  */
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if (getenv('APP_ENV') === 'development') {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+}
 
 session_start();
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
