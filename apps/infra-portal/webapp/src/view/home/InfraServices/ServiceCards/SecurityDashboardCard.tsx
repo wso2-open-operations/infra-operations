a47213119d4250ef6c04987007a4dd098d1a3f7a
ThaminduMiuranda <thaminduthilina@gmail.com>
2026-03-19 18:38:35 +0530

    fix(typography): comment out fontWeight property for GitHub and Security dashboard cards

--- apps/infra-portal/webapp/src/view/home/InfraServices/ServiceCards/SecurityDashboardCard.tsx
+++ apps/infra-portal/webapp/src/view/home/InfraServices/ServiceCards/SecurityDashboardCard.tsx
@@ -98,7 +98,7 @@ export default function SecurityDashboardCard() {
           <Typography
             sx={{
               fontSize: 14,
-              fontWeight: 600,
+              // fontWeight: 600,
               color: theme.palette.customText.primary.p1.active,
             }}
           >
